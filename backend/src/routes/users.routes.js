e0064842945b1fe04e5b5ca2a4af284c9a924a12
lrizza <leonardorizza@hotmail.com>
2023-12-24 12:57:00 -0300

    nodemailer con premium 2

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getUser, getUserById, putUser, deleteUser, passwordRecovery, resetPassToken, premiumUser } from "../controllers/users.controller.js";
+import { passportError, authorization } from "../utils/messagesError.js";
 
 const userRouter = Router()
 
@@ -11,6 +12,6 @@ userRouter.delete('/:id', deleteUser)
 userRouter.post('/password-recovery', passwordRecovery);
 userRouter.post('/reset-password/:token', resetPassToken);
 //premium
-userRouter.put('/:id/update-premium', premiumUser);
+userRouter.put('/:id/update-premium', passportError('jwt'), authorization('admin'), premiumUser);
 
 export default userRouter
