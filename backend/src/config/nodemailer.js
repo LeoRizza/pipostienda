a227aa1650c0a16c9b0cccd79dbf9c8fbb1966a1
lrizza <leonardorizza@hotmail.com>
2023-12-24 11:37:10 -0300

    nodemailer funcionando en bdd con dotenv

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer'
+import 'dotenv/config'
 
 const transport = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -6,15 +7,15 @@ const transport = nodemailer.createTransport({
     secure: true,
     service: 'gmail',
     auth: {
-        user: 'leonardor.prueba1@gmail.com',
-        pass: 'vmml pogl biaf sapl',
+        user: process.env.EMAIL_USER,
+        pass: process.env.PASSWORD_EMAIL,
         authMethod: 'LOGIN'
     }
 })
 
 export const sendRecoveryMail = (email, recoveryLink) => {
     const mailOptions = {
-        from: 'leonardor.prueba1@gmail.com',
+        from: process.env.EMAIL_USER,
         to: email,
         subject: 'Link para reestablecer su contraseña',
         text: `Haga click en el siguiente enlace para reestablecer su contraseña: ${recoveryLink}`
